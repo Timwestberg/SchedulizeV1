d6121a92bbe3a033798259711d755cbfe0a65c98
Entreri <tphakasoum@gmail.com>
2019-01-19 11:17:56 -0800

    added routes and app.js router dom

--- client/src/App.js
+++ client/src/App.js
@@ -1,28 +1,26 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { BrowserRouter as Router, Route, Switch } from "reaact-router-dom";
+import Login from "./pages/Login";
+import Calendar from "./pages/Calendar";
+import MapPage from "./pages/MapPage";
+import Address from "./pages/Address";
+import NoMatch from "./pages/NoMatch";
 
-class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
-    );
-  }
+function App() {
+  return (
+    <Router>
+    <div>
+      <Switch>
+        <Route exact path="/" component={Login} />
+        <Route exact path="/calendar" component={Calendar} />
+        <Route exact path="/map" component={MapPage} />
+        <Route exact path="/addressbook" component={Address} />
+        <Route component={NoMatch}/>
+
+      </Switch>
+    </div>
+    </Router>
+  );
 }
 
 export default App;
