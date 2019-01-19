d49a4e6f86e7198cd3b973ac9ce5547c71b33e9a
Entreri <tphakasoum@gmail.com>
2019-01-19 13:06:10 -0800

    adding routes for backend api

--- routes/api/schedulize.js
+++ routes/api/schedulize.js
@@ -1,6 +1,8 @@
 const router = require("express").Router();
 const clientsController = require("../../controllers/clientsController");
 const contractorsController = require("../../controllers/contractorController");
+const appointmentsController = require("../../controllers/appointmentsController");
+
 
 // Matches with "/api/books"
 router.route("/")
