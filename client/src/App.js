d84ea007b848ecfcfd19f6636b56c8afadd31e5e
Entreri <tphakasoum@gmail.com>
2019-01-19 20:27:30 -0800

    created routes to login page and form/card pages

--- client/src/App.js
+++ client/src/App.js
@@ -6,15 +6,17 @@ import MapPage from "./pages/MapPage";
 import Address from "./pages/Address";
 import NoMatch from "./pages/NoMatch";
 import Cards from "./pages/Cards";
+import Login from "./pages/Login";
 
 function App() {
   return (
     <Router>
     <div>
       <Switch>
-        <Route exact path="/" component={Cards} />
+        <Route exact path="/" component={Login} />
         <Route exact path="/calendar" component={Calendar} />
         <Route exact path="/map" component={MapPage} />
+        <Route exact path="/forms" component={Cards} />
         <Route exact path="/addressbook" component={Address} />
         <Route component={NoMatch}/>
       </Switch>
--- client/src/components/Navbar/index.js
+++ client/src/components/Navbar/index.js
@@ -8,7 +8,7 @@ function Navbar() {
         <li className="calendar"><a href="/calendar">Calendar</a></li>
         <li className="address"><a href="/addressbook">Address Book</a></li>
         <li className="map"><a href="/map">Map</a></li>
-        <li className="calendar"><a href="/forms">Forms</a></li>
+        <li className="forms"><a href="/forms">Forms</a></li>
         <li className="logout"><a href="/">Logout</a></li>
       </ul>
     </nav>
