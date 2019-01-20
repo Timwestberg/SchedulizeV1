541b3d458cb44d2452ea87cf7c5abece9d72860e
Entreri <tphakasoum@gmail.com>
2019-01-19 23:01:44 -0800

    deleted commented out code in navbar

--- client/src/components/Navbar/index.js
+++ client/src/components/Navbar/index.js
@@ -3,24 +3,8 @@ import "./style.css";
 
 function Navbar() {
   return (
-    // <nav className="nav">
-    //   <ul>
-    //     <li className="calendar"><a href="/calendar">Calendar</a></li>
-    //     <li className="address"><a href="/addressbook">Address Book</a></li>
-    //     <li className="map"><a href="/map">Map</a></li>
-    //     <li className="forms"><a href="/forms">Forms</a></li>
-    //     <li className="logout"><a href="/">Logout</a></li>
-    //     <form>
-    //     <input type="text" placeholder="Search.." name="search"/>
-    //     <button type="submit">Submit</button>
-    //     </form>
-    //   </ul>
-    //   <div className="search-container">
-
-    //   </div>
-    // </nav>
     <div className="topnav">
-  <a href="/">Home</a>
+  <a çhref="/">Home</a>
   <a href="/calendar">Calendar</a>
   <a href="/addressbook">Address Book</a>
   <a href="/map">Map</a>
