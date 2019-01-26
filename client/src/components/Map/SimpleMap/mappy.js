964cd266cfa952719d48526010c7bae078c85ac4
mpmks1 <miranda.swartwood@gmail.com>
2019-01-26 11:46:19 -0800

    added to /pages/MapPage, /components/Map - created simple map using new NPM package google-map-react

---
+++ client/src/components/Map/SimpleMap/mappy.js
@@ -0,0 +1,36 @@
+import React, { Component } from 'react';
+import GoogleMapReact from 'google-map-react';
+ 
+const AnyReactComponent = ({ text }) => <div>{text}</div>;
+ 
+class SimpleMap extends Component {
+  static defaultProps = {
+    center: {
+      lat: 32.71,
+      lng: -117.16
+    },
+    zoom: 11
+  };
+ 
+  render() {
+    return (
+      // Important! Always set the container height explicitly
+      <div style={{ height: '100vh', width: '100%' }}>
+        <GoogleMapReact
+          bootstrapURLKeys={{ key: 'AIzaSyBut6uSL-YAgTuek3nzJ4dcesUg8g42rnk' }}
+          defaultCenter={this.props.center}
+          defaultZoom={this.props.zoom}
+        >
+          <AnyReactComponent
+            lat={32.715736}
+            lng={	-117.161087}
+            // text={'San Diego'}
+          />
+        </GoogleMapReact>
+      </div>
+    );
+  }
+}
+ 
+export default SimpleMap;
+    
--- client/src/pages/Cards.js
+++ client/src/pages/Cards.js
@@ -2,7 +2,7 @@ import React, {
     Component
   } from 'react';
   import Appointmentform from "../components/AppointmentForm"
-  import ContractorCard from "../components/contractor/contractorCard"
+  import ContractorCard from "../components/contractor"
   import ApptCard from "../components/ApptCard"
   import Navbar from "../components/Navbar"
   // import './App.css';
@@ -14,7 +14,12 @@ import React, {
         div className = "App" >
         <Navbar/>
         <Appointmentform />
-  
+
+ // ContractorCard -  this is firing an error - ./src/pages/Cards.js
+// Module not found: Can't resolve '../components/contractor/contractorCard' 
+// in 
+// 'C:\Users\mpmks\Desktop\3\Schedulize\client\src\pages' 
+// UPDATED to file directory - '..components/contractor' -ms 1/22/2019
         <ContractorCard />
   
         <ApptCard />
