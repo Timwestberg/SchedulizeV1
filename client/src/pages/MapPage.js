0b8f65c9db6f1fccd6941b7f649c2970049beb16
Entreri <tphakasoum@gmail.com>
2019-01-30 13:08:15 -0800

    cleaned up MapPage code

--- client/src/pages/MapPage.js
+++ client/src/pages/MapPage.js
@@ -1,21 +1,3 @@
-// import React from 'react';
-// import MapPage from "../components/Map";
-// import Navbar from "../components/Navbar";
-// import TestMap from "../components/Map/TestMap"
-
-// function Googlemappy() {
-//   return (
-//       <div>
-//           <Navbar/>
-//          {/* <MapPage /> */}
-//          <TestMap />
-//       </div>
-//   )
-// }
-
-// export default Googlemappy;
-
-
 import React, { Component } from "react";
 import { Map, InfoWindow, Marker, GoogleApiWrapper } from 'google-maps-react';
 import API from "../utils/API";
