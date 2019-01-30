fc81006726b7bba81514240f4327f56c61ccf1e0
mpmks1 <miranda.swartwood@gmail.com>
2019-01-29 16:20:52 -0800

    tried many things... none working X'(

--- client/src/components/Map/SimpleMap/mappy.js
+++ client/src/components/Map/SimpleMap/mappy.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 import GoogleMapReact from 'google-map-react';
- 
+
 const AnyReactComponent = ({ text }) => <div>{text}</div>;
  
 class SimpleMap extends Component {
